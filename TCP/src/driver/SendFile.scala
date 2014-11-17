bf8d8b48ca7c8c8de7213c86dabe7b2584a38aa9
nathan-liyiming <nathan.liyiming@gmail.com>
2014-11-17 14:21:42 -0500

    test speed

--- TCP/src/driver/SendFile.scala
+++ TCP/src/driver/SendFile.scala
@@ -5,12 +5,16 @@ import tcp.TCP
 
 class SendFile(socket: Int, source: BufferedReader, tcp: TCP) extends Runnable {
   val BufSize = 1024
+  var count: Long = 0
 
   def run() {
     try {
+      val startTime = System.currentTimeMillis
+
       val buf = new Array[Char](BufSize)
       var readBytes = source.read(buf, 0, BufSize)
       while (readBytes != -1) {
+        count += readBytes.asInstanceOf[Long]
         val writeBytes = tcp.virWriteAll(socket, buf.map(_.toByte).toArray.slice(0, readBytes))
         readBytes = source.read(buf, 0, BufSize)
       }
@@ -18,8 +22,12 @@ class SendFile(socket: Int, source: BufferedReader, tcp: TCP) extends Runnable {
       tcp.virClose(socket)
       source.close
 
+      val endTime = System.currentTimeMillis
+
+      val effectiveBandwidth = count / (1024 * 1024 * ((endTime - startTime) / 1000.0))
+
       println("")
-      println("sendfile on socket " + socket + " done")
+      println("Sendfile on socket " + socket + " done, " + "effective bandwidth: " + f"$effectiveBandwidth%.1f")
       print("> ")
     } catch {
       case e: Exception => println(e.getMessage)
